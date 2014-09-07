32b6bd28ea8ba056a2143eed57b4667934bfdf46
Henrik Hedelund <henrik@improove.se>
2014-09-07 20:35:06 +0200

    Syntax highlight styles

---
+++ public/config.rb
@@ -0,0 +1,9 @@
+# Config environment [:production|:development]
+environment     = :production
+
+# Compass Configuration:
+http_path       = ''
+css_dir         = 'css'
+sass_dir        = 'scss'
+line_comments   = false
+
