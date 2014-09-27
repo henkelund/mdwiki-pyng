7d1eaf64dcc073759977424db1e9704c7075e1eb
Henrik Hedelund <henrik@improove.se>
2014-09-27 22:19:08 +0200

    Multi wiki support

--- mdwiki/__init__.py
+++ mdwiki/__init__.py
@@ -10,22 +10,43 @@ __copyright__ = 'Copyright 2014, Henrik Hedelund'
 __license__ = 'AGPL 3'
 __email__ = 'henke.hedelund@gmail.com'
 
-app = Factory.get_app()
 
-@app.route('/file/')
-@app.route('/file/<path:path>')
-def get_file(path=''):
-    """File controller"""
-    doc = Factory.get_mddoc(path)
-    return jsonify(doc.as_dict())
+class Dispatcher(object):
+    """Application dispatcher class"""
 
-@app.route('/search/<path:query>')
-def get_search_result(query):
-    """Search controller"""
-    searcher = Factory.get_searcher()
-    result = searcher.search(query + '*')
-    return jsonify({
-        'hits': result,
-        'correction': searcher.correct(query)
-    })
+    def __init__(self):
+        """Define contoller action routes"""
+        self._routes = (
+            ('/file/', 'file_action'),
+            ('/file/<path:path>','file_action'),
+            ('/search/<path:query>', 'search_action')
+        )
+
+    def _attach_routes(self, app):
+        """Attach controller actions to given app"""
+        for rule, endpoint in self._routes:
+            app.add_url_rule(rule, endpoint, getattr(self, endpoint))
+
+    def file_action(self, path=''):
+        """File controller"""
+        doc = Factory.get_mddoc(path)
+        return jsonify(doc.as_dict())
+
+    def search_action(self, query):
+        """Search controller"""
+        searcher = Factory.get_searcher()
+        result = searcher.search(query + '*')
+        return jsonify({
+            'hits': result,
+            'correction': searcher.correct(query)
+        })
+
+    def __call__(self, environ, start_response):
+        """Proxy callable for app instance"""
+        app = Factory.get_app(environ.get('MDWIKI_APP'))
+        if not app.got_first_request:
+            self._attach_routes(app)
+        return app(environ, start_response)
+
+app = Dispatcher()
 
--- mdwiki/factory.py
+++ mdwiki/factory.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 """Factory module"""
 
-from glob import glob
+from os import environ
 from os.path import abspath, join
-from flask import Flask
+from glob import glob
+from threading import Lock
+from flask import Flask, request
 import config as default_config
 from document import Document
 from markdown import MarkdownHtmlRenderer
@@ -15,42 +17,57 @@ __copyright__ = 'Copyright 2014, Henrik Hedelund'
 __license__ = 'AGPL 3'
 __email__ = 'henke.hedelund@gmail.com'
 
-class Factory:
+class Factory(object):
     """Factory class"""
 
-    _app = None
+    _apps = {}
+    _lock = Lock()
 
     @classmethod
-    def get_app(cls):
+    def get_app(cls, name=None):
         """Instantiate Flask app"""
-        if cls._app is None:
-            cls._app = Flask('mdwiki')
-            cls._app.config.from_object(default_config)
-            for pyfile in glob('config/*.py'):
-                cls._app.config.from_pyfile(abspath(pyfile))
-        return cls._app
+        if not name:
+            try:
+                name = request.environ.get('MDWIKI_APP')
+                if not name:
+                    raise RuntimeError('Empty app name')
+            except RuntimeError as e: # working outside of request context
+                name = 'mdwiki'
+        with cls._lock:
+            app = cls._apps.get(name)
+            if app is None:
+                app = Flask(name)
+                environ['MDWIKI_APP'] = name # expose app name to config files
+                app.config.from_object(default_config)
+                for pyfile in glob('config/*.py'):
+                    app.config.from_pyfile(abspath(pyfile))
+                del environ['MDWIKI_APP']
+                cls._apps[name] = app
+            return app
 
     @classmethod
     def get_mddoc(cls, path='', **kwargs):
         """Create a Markdown document from given path"""
-        config = cls.get_app().config
-        base_path = config['DOCUMENTS_PATH']
+        app = cls.get_app()
+        base_path = app.config.get('DOCUMENTS_PATH')
         if not 'file_pattern' in kwargs:
             kwargs['file_pattern'] = r'\.md$'
         if not 'content_renderer' in kwargs:
             kwargs['content_renderer'] = MarkdownHtmlRenderer(
-                config['MD_EXTENSIONS'],
-                config['MD_HTML_FLAGS']
+                app.config.get('MD_EXTENSIONS'),
+                app.config.get('MD_HTML_FLAGS')
             ).render
         return Document(join(base_path, path), **kwargs)
 
     @classmethod
     def get_searcher(cls, **kwargs):
         """Get searcher instance"""
-        config = cls.get_app().config
+        app = cls.get_app()
+        if not 'index' in kwargs:
+            kwargs['index'] = app.name
         if not 'base_path' in kwargs:
-            kwargs['base_path'] = config['DOCUMENTS_PATH']
+            kwargs['base_path'] = app.config.get('DOCUMENTS_PATH')
         if not 'default_limit' in kwargs:
-            kwargs['default_limit'] = config['SEARCH_LIMIT']
+            kwargs['default_limit'] = app.config.get('SEARCH_LIMIT')
         return MarkdownSearcher(**kwargs)
 
